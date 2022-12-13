c33fba23d7242f78f68d22a3e6cfd5f8766fea00
Daniel Peterson <1depeterson@seattleschools.org>
2022-12-13 09:44:34 -0800

    idk

--- Assets/Scripts/PlayerScriptAlpha.cs
+++ Assets/Scripts/PlayerScriptAlpha.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using Random = System.Random;
 
 public class PlayerScriptAlpha : MonoBehaviour {
 	public GameObject card;
 
     // Start is called before the first frame update
-    void Awake() {
-	    getInPosition(1, 3);
+	void Awake() {
+		//Random rand = new Random();
+		//getInPosition(rand.Next(5), 5);
     }
+
+	// Update is called once per frame
+	void Update() {
+	    
+	}
 
 	private void getInPosition(int playerNum, int numPlayers) {
 		transform.Rotate(0, playerNum * 360 / numPlayers, 0);
 		transform.Translate(0, 0, 7);
 		transform.Rotate(0, 180, 0);
 	}
-
-    // Update is called once per frame
-    void Update() {
-	    
-    }
 }
